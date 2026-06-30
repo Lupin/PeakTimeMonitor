4ec72d304cc80495ca4d7748ceeb210410f2dd51
Hermes <gael@hermes.local>
2026-06-30 20:25:42 +0200

    fix: MenuHandler = proxy ObjC dédié qui reçoit les actions du NSMenu via closures

--- PeakTimeMonitorApp/AppDelegate.swift
+++ PeakTimeMonitorApp/AppDelegate.swift
@@ -1,10 +1,27 @@
 import Cocoa
 
+/// Handler intermédiaire qui reçoit les actions du menu et les redirige
+private class MenuHandler: NSObject {
+    var onShow: (() -> Void)?
+    var onPrefs: (() -> Void)?
+    var onQuit: (() -> Void)?
+
+    @objc func show() { onShow?() }
+    @objc func prefs() { onPrefs?() }
+    @objc func quit() { onQuit?() }
+}
+
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
     private var timer: Timer?
+    private let menuHandler = MenuHandler()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
+        // Configurer les actions via closures
+        menuHandler.onShow = { [weak self] in self?.showWindow() }
+        menuHandler.onPrefs = { [weak self] in self?.openPrefs() }
+        menuHandler.onQuit = { NSApp.terminate(nil) }
+
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
         if let button = statusItem.button {
             button.image = makeFeuIcon(state: currentState())
@@ -12,16 +29,11 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         }
 
         let menu = NSMenu()
-        let showItem = NSMenuItem(title: "Afficher", action: #selector(showWindow), keyEquivalent: "")
-        showItem.target = self
-        menu.addItem(showItem)
-        let prefsItem = NSMenuItem(title: "Préférences", action: #selector(openPrefs), keyEquivalent: ",")
-        prefsItem.target = self
-        menu.addItem(prefsItem)
+        menu.addItem(NSMenuItem(title: "Afficher", action: #selector(MenuHandler.show), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Préférences", action: #selector(MenuHandler.prefs), keyEquivalent: ","))
         menu.addItem(.separator())
-        let quitItem = NSMenuItem(title: "Quitter", action: #selector(quitApp), keyEquivalent: "q")
-        quitItem.target = self
-        menu.addItem(quitItem)
+        menu.addItem(NSMenuItem(title: "Quitter", action: #selector(MenuHandler.quit), keyEquivalent: "q"))
+        menu.items.forEach { $0.target = menuHandler }
         statusItem.menu = menu
 
         timer = Timer.scheduledTimer(withTimeInterval: 30, repeats: true) { [weak self] _ in
@@ -33,33 +45,31 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { false }
 
-    @objc func showWindow() {
+    private func showWindow() {
         NSApp.setActivationPolicy(.regular)
         NSApp.activate(ignoringOtherApps: true)
         NSApp.windows.first?.makeKeyAndOrderFront(nil)
     }
 
-    @objc func openPrefs() {
+    private func openPrefs() {
         NSApp.activate(ignoringOtherApps: true)
         if NSApp.responds(to: NSSelectorFromString("showSettingsWindow:")) {
             NSApp.perform(NSSelectorFromString("showSettingsWindow:"))
         }
     }
 
-    @objc func quitApp() { NSApp.terminate(nil) }
-
-    func updateIcon() {
+    private func updateIcon() {
         statusItem.button?.image = makeFeuIcon(state: currentState())
     }
 
-    func currentState() -> FeuState {
+    private func currentState() -> FeuState {
         let d = UserDefaults(suiteName: "group.peakmonitor")
         let slots = d?.peakTimeSlots ?? PeakTimeSlot.defaultSlots
         let om = d?.integer(forKey: "orangeMinutes") ?? 15
         return PeakTimeSlot.currentState(slots: slots, orangeMinutes: om > 0 ? om : 15)
     }
 
-    func makeFeuIcon(state: FeuState) -> NSImage {
+    private func makeFeuIcon(state: FeuState) -> NSImage {
         let img = NSImage(size: NSSize(width: 18, height: 18))
         img.isTemplate = true
         img.lockFocus()
